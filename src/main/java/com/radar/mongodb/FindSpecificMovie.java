71125117ab1e029e43e52ae0ac4f08eca9bee8e3
Ricardo De la Rosa <rdlarosa2@gmail.com>
2025-09-06 22:29:47 -0500

    Second commit

---
+++ src/main/java/com/radar/mongodb/FindSpecificMovie.java
@@ -0,0 +1,26 @@
+package com.radar.mongodb;
+
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class FindSpecificMovie {
+
+    public static void main( String[] args ) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://rdlarosa2_db_user:V4ll4v4c4@cluster0.c8l7t3g.mongodb.net/";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");wq
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
